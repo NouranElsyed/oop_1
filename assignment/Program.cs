7c5d676ad37ea2aaa5e141c3b36ab508eefe687d
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-07-08 19:36:35 +0300

    Q1 & Q2

---
+++ assignment/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace assignment
+{
+    public enum WeekDays
+    {
+
+        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    }
+
+    public enum session
+    {
+
+        Spring, Summer, Autumn, Winter
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region Q1
+            //foreach (int i in Enum.GetValues(typeof(WeekDays)))
+            //{
+            //    Console.WriteLine((WeekDays)i);
+            //}
+
+            #endregion
+
+            #region Q2
+            //string? name;
+            //do {name = Console.ReadLine(); } while (name == null);
+
+            //switch (name.ToLower())
+            //{
+            //    case "march":
+            //    case "april":
+            //    case "may":
+            //        Console.WriteLine(session.Spring);
+            //        break;
+            //    case "june":
+            //    case "july":
+            //    case "august ":
+
+            //        Console.WriteLine(session.Summer);
+            //        break;
+            //    case "september":
+            //    case "october":
+            //    case "november ":
+            //        Console.WriteLine(session.Autumn);
+            //        break;
+            //    case "december ":
+            //    case "january":
+            //    case "february ":
+            //        Console.WriteLine(session.Winter);
+            //        break;
+            //    default :
+            //        Console.WriteLine("that is not a month name.");
+            //        break;
+            //}
+
+            #endregion
+
+            #region Q3
+
+
+            #endregion
+
+            #region Q4
+
+
+            #endregion 
+            #region Q5
+
+
+            #endregion
+        }
+    }
+}
